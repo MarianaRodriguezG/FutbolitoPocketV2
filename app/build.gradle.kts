f81546eaea45037db9981cb9f291ba774188b58b
MarianaRodriguezG <s21120236@alumnos.itsur.edu.mx>
2025-04-03 22:03:20 -0600

    Nuevas modificaciones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    //Se agrega la implementacion de la libreria de rick nout
+    implementation(libs.composesensors)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
